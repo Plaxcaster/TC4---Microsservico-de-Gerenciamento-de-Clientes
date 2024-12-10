66d6785cb4ecbc1db24d6c38dc3e6aa9828a68f1
Pedro Plá <pedro.bpla@gmail.com>
2024-12-10 17:22:17 -0300

    Implementado teste de integração

--- src/main/java/horizonleap/gerenciamento/clientes/controller/ClienteController.java
+++ src/main/java/horizonleap/gerenciamento/clientes/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import horizonleap.gerenciamento.clientes.model.ClienteModel;
@@ -23,26 +24,24 @@ public class ClienteController {
     @Operation(summary = "Consultar dados do Cliente", tags = { "Consulta", "Cliente" })
     @GetMapping("/{id_cliente}")
     public ResponseEntity<ClienteModel> busca(@PathVariable Integer id_cliente) {
-
         return ResponseEntity.ok(service.busca(id_cliente));
-
     }
 
     @Operation(summary = "Consultar dados do Cliente e seus carros", tags = { "Consulta", "Cliente" })
     @GetMapping("/dadosCliente/{id_cliente}")
     public ResponseEntity<DadosClienteDTO> consultarDadosCliente(@PathVariable Integer id_cliente) {
-
         return ResponseEntity.ok(service.consultarDadosCliente(id_cliente));
     }
 
     @Operation(summary = "Altera endereço do cliente", tags = { "Alteração", "Cliente" })
-    @PutMapping("/{endereco}")
-    public ResponseEntity<ClienteModel> alteraEndereco(@PathVariable String id_cliente,  String endereco) {
-
+    @PutMapping("/{id_cliente}/endereco")
+    public ResponseEntity<ClienteModel> alteraEndereco(
+            @PathVariable Integer id_cliente,
+            @RequestParam String endereco) {
         try {
-            Integer idCliente = Integer.valueOf(id_cliente);
-
-            ClienteModel cliente = service.updateEndereco(idCliente);
+            ClienteModel cliente = service.updateEndereco(id_cliente);
+            cliente.setEndereco(endereco);
+            service.save(cliente.getNome(), cliente.getEndereco(), cliente.getInfoContato(), cliente.getCpf(), cliente.getEnderecoUF());
             return ResponseEntity.ok(cliente);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new ClienteModel());
